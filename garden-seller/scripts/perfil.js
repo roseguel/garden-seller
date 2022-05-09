9f5d64d58f80bf58097d94edeb5a5d7c10422d93
Ariel Silva <51482182+Alwexis@users.noreply.github.com>
2022-05-08 21:57:38 -0400

    Delete garden-seller directory

--- garden-seller/scripts/perfil.js
+++
@@ -1,9 +0,0 @@
-foto = new DataTransfer();
-
-function cambiarFoto() {
-    imagen = document.getElementById("imagenForm").files.item(0)
-    url = URL.createObjectURL(imagen);
-    document.getElementById("sinFotoDePerfil").style.display = "none";
-    document.getElementById("fotoDePerfil").src = url;
-    document.getElementById("conFotoDePerfil").style.display = "flex";
-}
